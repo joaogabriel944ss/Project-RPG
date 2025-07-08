1471bfc5c3ac212ac498ef7c33e4e56ad6f45612
Joao-Gabriel <joaogabriel944ss@gmail.com>
2025-07-08 03:57:28 -0300

    Version 0.1.0 - 08/07/2025 at 03:47 action: [creating the first css and html and starting the javascript script for dice rolls] done by: [João Gabriel]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,24 @@ Todas as mudanças notáveis neste projeto serão documentadas aqui.
 O formato é baseado em [Keep a Changelog](https://keepachangelog.com/pt-BR/1.0.0/)
 e este projeto segue o [Versionamento Semântico](https://semver.org/lang/pt-BR/).
 
+
+
+
 ---
+
+
+## [0.1.0] - 2025-07-08
+### Adicionado
+- Criação da estrutura inicial em HTML.
+- Estilização inicial com CSS.
+- Início do script em JavaScript para simular rolagem de dados.
+
+**Desenvolvido por:** João Gabriel
+
+
+
+---
+
 
 ## [0.0.1] - 2025-07-03
 ### Adicionado
@@ -15,6 +32,8 @@ e este projeto segue o [Versionamento Semântico](https://semver.org/lang/pt-BR/
 - Botões para simular rolagem de dados (d6, d8, d10, d12, d20, d100).
 - Inclusão dos arquivos de estilo (`style.css`) e script (`script.js`).
 - Título da página e layout básico inicial.
+
+
 
 ---
 
---
+++ script.js
@@ -0,0 +1,40 @@
+
+document.getElementById("d4").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 4) + 1;
+  window.alert(resultado);
+});
+
+document.getElementById("d6").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 6) + 1;
+  window.alert(resultado);
+});
+
+
+document.getElementById("d8").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 8) + 1;
+  window.alert(resultado);
+});
+
+
+document.getElementById("d10").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 8) + 1;
+  window.alert(resultado);
+});
+
+
+document.getElementById("d12").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 8) + 1;
+  window.alert(resultado);
+});
+
+
+document.getElementById("d20").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 20) + 1;
+  window.alert(resultado);
+});
+
+
+document.getElementById("d100").addEventListener("click", function () {
+  const resultado = Math.floor(Math.random() * 100) + 1;
+  window.alert(resultado);
+});
